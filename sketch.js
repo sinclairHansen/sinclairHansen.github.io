5b86c645782831d20c6a16f971cce3aedbc94696
Sinclair Hansen <162069768+sinclairHansen@users.noreply.github.com>
2025-04-24 23:08:07 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,76 @@
+let g = 1;
+let pendulums = [];
+let width = 500;
+let height = 500;
+let colorPalette = ['#4a4e4d','#0e9aa7','#3da4ab','#f6cd61','#fe8a71'];
+
+
+//This function creates the sliders on user interface
+  function setupSlider(rangeId, outputId) {
+      var slider = document.getElementById(rangeId);
+      var output = document.getElementById(outputId);
+      output.innerHTML = slider.value;
+      slider.oninput = function() {
+        output.innerHTML = this.value;
+      };
+    }
+
+
+  // Setup all sliders
+  document.addEventListener("DOMContentLoaded", function () {
+    setupSlider("L1", "output1");
+    setupSlider("L2", "output2");
+    setupSlider("m1", "output3");
+    setupSlider("m2", "output4");
+    setupSlider("num", "output5");
+    setupSlider("trail", "output6");
+  }
+
+  );
+
+
+
+    function setup()
+    {
+      this.num = parseFloat(document.getElementById("num").value);
+      createCanvas(width,height);
+      for(i = 0; i<num; i++)
+        {
+          //This grabs the values the user sets in the sliders
+          let L1 = parseFloat(document.getElementById("L1").value);
+          let L2 = parseFloat(document.getElementById("L2").value);
+          let m1 = parseFloat(document.getElementById("m1").value);
+          let m2 = parseFloat(document.getElementById("m2").value); 
+          let theta1 = PI/2 + (0.001)*i;
+          let theta2 = PI/4;
+          pendulums[i] = new Pendulum(theta1,theta2, L1, L2, m1, m2);
+        }
+
+    draw();
+    }
+  
+
+
+  function draw()
+  {
+    this.trail= parseFloat(document.getElementById("trail").value); 
+    //If I do background(0,30) it will make a trail along the pendulums
+    //due to it changing the transparancy
+    //Maybe make it an option later on.--> background(0,x)
+      background(0,trail);
+      for(i = 0; i<num; i++)
+        {
+          pendulums[i].update();
+          pendulums[i].display();
+        }
+
+  }
+
+
+
+
+
+
+
+
+
